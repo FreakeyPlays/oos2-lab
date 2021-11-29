a8e711f4130d43a70f0ddaaf9901374ad2b2c233
chmeit02 <chmeit02@hs-esslingen.de>
2021-11-29 15:56:55 +0100

    Finished Lab 5 and 6

--- src/acamo/Acamo.java
+++ src/acamo/Acamo.java
@@ -1,17 +1,32 @@
 package acamo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+import de.saring.leafletmap.LatLong;
+import de.saring.leafletmap.LeafletMapView;
+import de.saring.leafletmap.MapConfig;
+import de.saring.leafletmap.MapLayer;
+import de.saring.leafletmap.Marker;
+import de.saring.leafletmap.ScaleControlConfig;
+import de.saring.leafletmap.ZoomControlConfig;
+import de.saring.leafletmap.events.MapClickEventListener;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Worker;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -20,24 +35,32 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import jsonstream.PlaneDataServer;
-import messer.BasicAircraft;
-import messer.Messer;
-import observer.Observable;
-import observer.Observer;
+import messer.*;
+import observer.*;
 import senser.Senser;
 
 public class Acamo extends Application implements Observer<BasicAircraft> {
     // Plane Data variables
     private static double latitude = 48.7433425;
     private static double longitude = 9.3201122;
+    private static int dist = 50;
     private static boolean haveConnection = true;
     private ActiveAircrafts activeAircrafts;
+    private PlaneDataServer server;
 
     // JavaFX Table
     private TableView<BasicAircraft> table = new TableView<BasicAircraft>();
     private ObservableList<BasicAircraft> aircraftList = FXCollections.observableArrayList();
-    ArrayList<Label> valueLabel = new ArrayList<Label>();
+    private ArrayList<Label> valueLabel = new ArrayList<Label>();
     private int selectedTableRow = -1;
+
+    // Map
+    private LeafletMapView map = new LeafletMapView();
+    private Marker homeMarker;
+    private HashMap<String, Marker> aircraftHashMap = new HashMap<String, Marker>();
+    private CompletableFuture<Worker.State> loadingState;
+    private TextField inputLong;
+    private TextField inputLat;
 
     public static void main(String[] args) {
         launch(args);
@@ -46,12 +69,11 @@ public class Acamo extends Application implements Observer<BasicAircraft> {
     @Override
     public void start(Stage primaryStage) {
         String urlString = "https://opensky-network.org/api/states/all";
-        PlaneDataServer server;
 
         if (haveConnection)
-            server = new PlaneDataServer(urlString, latitude, longitude, 150);
+            server = new PlaneDataServer(urlString, latitude, longitude, 50);
         else
-            server = new PlaneDataServer(latitude, longitude, 100);
+            server = new PlaneDataServer(latitude, longitude, 50);
 
         Senser senser = new Senser(server);
         new Thread(server).start();
@@ -75,12 +97,33 @@ public class Acamo extends Application implements Observer<BasicAircraft> {
             public void run() {
                 aircraftList.clear();
                 aircraftList.addAll(activeAircrafts.values());
-                if (selectedTableRow >= 0) {
-                    table.getSelectionModel().select(selectedTableRow);
-                    BasicAircraft bac = table.getSelectionModel().getSelectedItem();
-                    ArrayList<Object> values = BasicAircraft.getAttributesValues(bac);
-                    for (int i = 0; i < values.size(); i++) {
-                        valueLabel.get(i).setText(values.get(i).toString());
+                try {
+                    if (selectedTableRow >= 0) {
+                        table.getSelectionModel().select(selectedTableRow);
+                        BasicAircraft bac = table.getSelectionModel().getSelectedItem();
+                        ArrayList<Object> values = BasicAircraft.getAttributesValues(bac);
+                        for (int i = 0; i < values.size(); i++) {
+                            valueLabel.get(i).setText(values.get(i).toString());
+                        }
+                    }
+                } catch (NullPointerException e) {
+                }
+
+                for (BasicAircraft bac : activeAircrafts.values()) {
+                    int direction = (int) bac.getTrak();
+                    LatLong coord = new LatLong(bac.getCoordinate().getLatitude(), bac.getCoordinate().getLongitude());
+                    String icao = bac.getIcao();
+                    String icon = "plane" + String.format("%02d", direction / 15);
+                    if (aircraftHashMap.containsKey(icao)) {
+                        Marker plane = aircraftHashMap.get(icao);
+                        plane.move(coord);
+                        plane.changeIcon(icon);
+                    } else {
+                        loadingState.whenComplete((state, throwable) -> {
+                            Marker plane = new Marker(coord, icao, icon, 0);
+                            map.addMarker(plane);
+                            aircraftHashMap.put(icao, plane);
+                        });
                     }
                 }
             }
@@ -88,6 +131,60 @@ public class Acamo extends Application implements Observer<BasicAircraft> {
     }
 
     private void javafx(Stage primaStage) {
+
+        // Map
+        Label mapTitle = createTitle("Map", new Insets(0, 0, 20, 0), 24);
+        List<MapLayer> config = new LinkedList<>();
+        config.add(MapLayer.OPENSTREETMAP);
+        loadingState = map.displayMap(new MapConfig(config, new ZoomControlConfig(),
+                new ScaleControlConfig(), new LatLong(latitude, longitude)));
+
+        loadingState.whenComplete((State, Throwable) -> {
+            // map.onMapMove(new MapMoveEventListener() { Frage???
+
+            // @Override
+            // public void onMapMove(LatLong arg0) {
+            // map.panTo(arg0);
+            // }
+
+            // });
+
+            map.onMapClick(new MapClickEventListener() {
+
+                @Override
+                public void onMapClick(LatLong coord) {
+                    inputLat.setText(String.valueOf(coord.getLatitude()));
+                    inputLong.setText(String.valueOf(coord.getLongitude()));
+                    switchLocation(coord.getLatitude(), coord.getLongitude(), dist);
+                }
+
+            });
+
+            homeMarker = new Marker(new LatLong(latitude, longitude), "Home", "Home", 1);
+            map.addCustomMarker("Home", "icons/basestationlarge.png");
+            map.addMarker(homeMarker);
+
+            for (int i = 0; i <= 24; i++) {
+                String n = String.format("%02d", i);
+                map.addCustomMarker("plane" + n, "icons/plane" + n + ".png");
+            }
+        });
+
+        for (BasicAircraft bac : activeAircrafts.values()) {
+            int direction = (int) bac.getTrak();
+            LatLong coord = new LatLong(bac.getCoordinate().getLatitude(), bac.getCoordinate().getLongitude());
+            String icao = bac.getIcao();
+            String icon = "plane" + String.format("%02d", direction / 15);
+            loadingState.whenComplete((state, throwable) -> {
+                Marker plane = new Marker(coord, icao, icon, 0);
+                map.addCustomMarker(icon, "icons/" + icon + ".png");
+                map.addMarker(plane);
+                aircraftHashMap.put(icao, plane);
+            });
+        }
+
+        map.setMaxWidth(500);
+        map.setMaxHeight(400);
 
         // Table
         Label tabelTitle = createTitle("Active Aircrafts", new Insets(0, 0, 20, 0), 24);
@@ -100,6 +197,7 @@ public class Acamo extends Application implements Observer<BasicAircraft> {
         table.setItems(aircraftList);
         table.setEditable(false);
 
+        // Data Pane
         GridPane selectedAircraftPane = new GridPane();
         selectedAircraftPane.setHgap(20);
         Label selectedTitle = createTitle("Selected Aircraft", new Insets(0, 0, 20, 0), 24);
@@ -115,38 +213,66 @@ public class Acamo extends Application implements Observer<BasicAircraft> {
             selectedAircraftPane.add(valueLabel.get(i), 1, i);
         }
 
+        // Input and Button for switching location
+        Label latLabel = createTitle("Latitude:", new Insets(5, 0, 5, 0), 14);
+        inputLat = new TextField();
+        inputLat.setText(String.valueOf(latitude));
+
+        Label lngLabel = createTitle("Longitude:", new Insets(5, 0, 5, 0), 14);
+        inputLong = new TextField();
+        inputLong.setText(String.valueOf(longitude));
+
+        Button submit = new Button("Submit");
+        submit.setOnAction(e -> {
+            switchLocation(Double.parseDouble(inputLat.getText()), Double.parseDouble(inputLong.getText()), dist);
+        });
+
         // Container for all Panes
         HBox container = new HBox();
+        VBox mapBox = new VBox();
         VBox tableBox = new VBox();
         VBox selectedBox = new VBox();
 
-        tableBox.setPadding(new Insets(0, 10, 0, 0));
+        mapBox.setPadding(new Insets(0, 10, 0, 0));
+        mapBox.getChildren().addAll(mapTitle, map, latLabel, inputLat, lngLabel, inputLong, submit);
+
+        tableBox.setPadding(new Insets(0, 10, 0, 10));
         tableBox.getChildren().addAll(tabelTitle, table);
 
         selectedBox.setPadding(new Insets(0, 0, 0, 10));
         selectedBox.getChildren().addAll(selectedTitle, selectedAircraftPane);
 
         container.setPadding(new Insets(20, 20, 20, 20));
-        container.getChildren().addAll(tableBox, selectedBox);
+        container.getChildren().addAll(mapBox, tableBox, selectedBox);
 
         // Eventhandler: selected row
-        table.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                selectedTableRow = table.getSelectionModel().getSelectedIndex();
+        rowSelectedEvent();
 
-                BasicAircraft bac = table.getSelectionModel().getSelectedItem();
-                ArrayList<Object> values = BasicAircraft.getAttributesValues(bac); // ERROR
-                for (int i = 0; i < values.size(); i++) {
-                    valueLabel.get(i).setText(values.get(i).toString());
-                }
-            }
-        });
-
-        Scene scene = new Scene(container, 800, 500);
+        Scene scene = new Scene(container, 1310, 600);
         primaStage.setOnCloseRequest(e -> System.exit(0));
         primaStage.setScene(scene);
         primaStage.setTitle("Acamo");
         primaStage.show();
+    }
+
+    public void rowSelectedEvent() {
+        table.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            public void handle(MouseEvent e) {
+                try {
+
+                    selectedTableRow = table.getSelectionModel().getSelectedIndex();
+
+                    BasicAircraft bac = table.getSelectionModel().getSelectedItem();
+                    ArrayList<Object> values = BasicAircraft.getAttributesValues(bac); // ERROR
+                    for (int i = 0; i < values.size(); i++) {
+                        valueLabel.get(i).setText(values.get(i).toString());
+                    }
+                } catch (NullPointerException ex) {
+
+                }
+            }
+        });
     }
 
     public Label createTitle(String s, Insets i, int f) {
@@ -155,5 +281,16 @@ public class Acamo extends Application implements Observer<BasicAircraft> {
         label.setFont(new Font(f));
 
         return label;
+    }
+
+    public void switchLocation(double lat, double lng, int dist) {
+        map.panTo(new LatLong(lat, lng));
+        homeMarker.move(new LatLong(lat, lng));
+        activeAircrafts.clear();
+        aircraftList.clear();
+        for (Marker mark : aircraftHashMap.values()) {
+            map.removeMarker(mark);
+        }
+        server.resetLocation(lat, lng, dist);
     }
 }
--- src/messer/BasicAircraftFactory.java
+++ src/messer/BasicAircraftFactory.java
@@ -9,7 +9,7 @@ import senser.AircraftSentence;
 public class BasicAircraftFactory {
     public BasicAircraft createAircraft(AircraftSentence as) {
         JSONArray data = as.getData();
-        Coordinate coordinate = new Coordinate(data.getDouble(5), data.getDouble(6));
+        Coordinate coordinate = new Coordinate(data.getDouble(6), data.getDouble(5));
         BasicAircraft ac = new BasicAircraft(data.getString(0), data.getString(1), new Date(data.getLong(4) * 1000),
                 coordinate, data.getDouble(9), data.getDouble(10));
 
